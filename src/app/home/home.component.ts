08f4f458b375edb735c5d57739af2adfe8266bfd
Felipe Teles <fliptm95@gmail.com>
2018-08-23 16:34:06 -0300

    Inclusão de OfertasService e instanciação na home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit,  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    const ofertas: OfertasService = new OfertasService();
-    console.log(ofertas.getOfertas());
+    console.log(this.ofertasService.getOfertas());
   }
 
 }
