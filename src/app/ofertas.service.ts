8eea38c83f820e208a30831b9d76002c17dc678a
Felipe Teles <fliptm95@gmail.com>
2018-09-11 16:50:23 -0300

    Criando modulo de constantes de API's

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,20 +2,22 @@ import { Oferta } from './shared/oferta.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasService {
+
   constructor(private httpc: HttpClient) {}
 
   public getOfertas(): Observable<Oferta[]> {
-    return this.httpc.get<Oferta[]>('http://localhost:3000/ofertas?destaque=true');
+    return this.httpc.get<Oferta[]>(`${URL_API}?destaque=true`);
   }
 
   public getOfertasPorCategoria(categoria: string): Observable<Oferta[]> {
-    return this.httpc.get<Oferta[]>(`http://localhost:3000/ofertas?categoria=${categoria}`);
+    return this.httpc.get<Oferta[]>(`${URL_API}?categoria=${categoria}`);
   }
 
   public getOfertasPorId(id: number): Observable<Oferta[]> {
-    return this.httpc.get<Oferta[]>(`http://localhost:3000/ofertas?id=${id}`);
+    return this.httpc.get<Oferta[]>(`${URL_API}?id=${id}`);
   }
 }
