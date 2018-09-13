6bc8af2758415337f8f2e393301e0ae5507bb4e1
Felipe Teles <fliptm95@gmail.com>
2018-09-13 16:49:40 -0300

    recuperando dados com http request como-usar

--- src/app/oferta/como-usar/como-usar.component.ts
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../../ofertas.service';
 
 @Component({
   selector: 'app-como-usar',
   templateUrl: './como-usar.component.html',
-  styleUrls: ['./como-usar.component.css']
+  styleUrls: ['./como-usar.component.css'],
+  providers: [ OfertasService]
 })
 export class ComoUsarComponent implements OnInit {
 
-  constructor() { }
+  public comoUsar = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) { }
 
   ngOnInit() {
+    this.ofertasService.getComoUsarOfertaPorId( this.route.parent.snapshot.params['id'] )
+    .then((resposta: any) => {
+      console.log(resposta);
+    });
   }
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -10,14 +10,22 @@ export class OfertasService {
   constructor(private httpc: HttpClient) {}
 
   public getOfertas(): Observable<Oferta[]> {
-    return this.httpc.get<Oferta[]>(`${URL_API}?destaque=true`);
+    return this.httpc.get<Oferta[]>(`${URL_API}/ofertas?destaque=true`);
   }
 
   public getOfertasPorCategoria(categoria: string): Observable<Oferta[]> {
-    return this.httpc.get<Oferta[]>(`${URL_API}?categoria=${categoria}`);
+    return this.httpc.get<Oferta[]>(`${URL_API}/ofertas?categoria=${categoria}`);
   }
 
   public getOfertasPorId(id: number): Observable<Oferta[]> {
-    return this.httpc.get<Oferta[]>(`${URL_API}?id=${id}`);
+    return this.httpc.get<Oferta[]>(`${URL_API}/ofertas?id=${id}`);
+  }
+
+  public getComoUsarOfertaPorId(id: number): Promise<string> {
+    return this.httpc.get(`${URL_API}/como-usar?id=${id}`)
+    .toPromise()
+    .then((resposta: any) => {
+      return resposta.json()[0].descricao;
+    });
   }
 }
